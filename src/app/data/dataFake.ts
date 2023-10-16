0ee4b22d6f1154622e5def1edf38596061b449c8
Prates <guipratesdsantos@gmail.com>
2023-10-16 17:56:02 -0300

    Deixando componentes dinamicos com typescript

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photoCover": "https://cdn.jornaldebrasilia.com.br/wp-content/uploads/2023/07/04102135/piymn8edskg.jpg",
+        "title":"Novo meta-critic revelado em OW2",
+        "description":"Meta critic de Overwatch 2 chega a 5.5 devido falta de colaboração com a comunidade."
+    },
+    {
+        "id":"2",
+        "photoCover": "https://img.youtube.com/vi/GE74zkzunZw/maxresdefault.jpg",
+        "title":"Novo evento de colaboração ao jogo Diablo 4 em OW2!",
+        "description":"A BLIZZARD começa a nova temporada de overwatch 2 com um novo evento de dia das bruxas, realizando uma colaboração com Diablo 4 (Um de seus jogos mais populares)"
+    },
+]
